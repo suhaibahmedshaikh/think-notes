1c44cc6257b30c70d13cd4afa7a1446208558511
Suhaib Ahmed Shaikh <suhaibshakilshaikh@gmail.com>
2025-08-02 02:59:14 +0530

    deployment preparation

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,11 +2,21 @@ import express from "express";
 import cors from "cors";
 import notesRouter from "./routes/notes.route.js";
 import rateLimiter from "./middlewares/rateLimiter.js";
+import path from "path";
+import { NODE_ENV } from "./configs/envConfig.js";
 
 const app = express();
+const __dirname = path.resolve();
 
 // middlewares
-app.use(cors());
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(rateLimiter);
@@ -14,9 +24,17 @@ app.use(rateLimiter);
 // routes
 app.use("/api/notes", notesRouter);
 
+if (NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("/:any", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
 // health check route
-app.get("/", (req, res) => {
-  res.send("Note Api is running");
-});
+// app.get("/", (req, res) => {
+//   res.send("Note Api is running");
+// });
 
 export default app;
--- backend/src/configs/envConfig.js
+++ backend/src/configs/envConfig.js
@@ -4,7 +4,14 @@ dotenv.config();
 
 const PORT = process.env.PORT;
 const MONGO_URI = process.env.MONGO_URI;
+const NODE_ENV = process.env.NODE_ENV;
 const UPSTASH_REDIS_REST_URL = process.env.UPSTASH_REDIS_REST_URL;
 const UPSTASH_REDIS_REST_TOKEN = process.env.UPSTASH_REDIS_REST_TOKEN;
 
-export { PORT, MONGO_URI, UPSTASH_REDIS_REST_URL, UPSTASH_REDIS_REST_TOKEN };
+export {
+  PORT,
+  MONGO_URI,
+  NODE_ENV,
+  UPSTASH_REDIS_REST_URL,
+  UPSTASH_REDIS_REST_TOKEN,
+};
